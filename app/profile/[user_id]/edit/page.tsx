f964604279455b1f0f2e6cab55cc041af8343bd2
jccdev45 <jccdev45@gmail.com>
2024-10-10 14:09:02 -0400

    refactor: :art: Refactor UserProfileForm
    - Improve composition: extract into FormFields & FormActions - Swap AuthForm for UserProfileForm on /profile/[user_id]/edit

--- app/profile/[user_id]/edit/page.tsx
+++ app/profile/[user_id]/edit/page.tsx
@@ -1,11 +1,9 @@
 import Image from "next/image"
 import { redirect } from "next/navigation"
-import { createClient } from "@/supabase/server"
 
 import { GradientBanner } from "@/components/gradient-banner"
 import { UserProfileForm } from "@/components/user-profile-form"
 import { getUser } from "@/app/(auth)/actions"
-import { AccountForm } from "@/app/profile/[user_id]/edit/account-form"
 
 import AccountInfoSvg from "/public/images/AccountInfo.svg"
 
@@ -22,7 +20,7 @@ export default async function EditProfilePage() {
 
   return (
     <>
-      <GradientBanner pattern text="Edit Profile" />
+      <GradientBanner pattern />
 
       <section className="grid -translate-y-16 grid-cols-1 gap-4 p-8 md:-translate-y-32 md:px-16 lg:grid-cols-2">
         <Image
@@ -33,8 +31,7 @@ export default async function EditProfilePage() {
           className="order-last col-span-1 mx-auto w-5/6 lg:order-first"
         />
 
-        {/* <UserProfileForm title="Edit Profile" formType="edit" userData={user} /> */}
-        <AccountForm user={user} />
+        <UserProfileForm title="Edit Profile" formType="edit" userData={user} />
       </section>
     </>
   )
--- components/user-profile-form.tsx
+++ components/user-profile-form.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import Link from "next/link"
-import { User } from "@supabase/supabase-js"
 
 import { registerFormItems } from "@/lib/constants"
 import { cn } from "@/lib/utils"
@@ -23,9 +22,22 @@ import { Typography } from "@/components/ui/typography"
 import { signup, updateProfile } from "@/app/(auth)/actions"
 import { AuthButton } from "@/app/(auth)/auth-button"
 
+import type { User } from "@supabase/supabase-js"
+
 interface UserProfileFormProps {
   title: string
   formType: "register" | "edit"
+  userData?: User
+}
+
+interface FormFieldsProps {
+  formType: "register" | "edit"
+  userData?: User
+}
+
+interface FormActionsProps {
+  formType: "register" | "edit"
+  title: string
   userData?: User
 }
 
@@ -34,98 +46,126 @@ export function UserProfileForm({
   formType,
   userData,
 }: UserProfileFormProps) {
-  const formItems =
-    formType === "edit" ? registerFormItems.slice(1, -1) : registerFormItems
-
-  const renderFormFields = () =>
-    formItems.map(
-      ({
-        type,
-        fieldName,
-        placeholder,
-        label,
-        required = formType === "edit" ? false : true,
-      }) => {
-        const value =
-          formType === "edit"
-            ? userData?.user_metadata[fieldName]
-            : fieldName === "email"
-              ? userData?.email
-              : ""
-
-        return (
-          <div
-            key={fieldName}
-            className={cn(
-              "col-span-2 space-y-1 *:text-lg md:col-span-1",
-              fieldName === "email" && "md:col-span-2"
-            )}
-          >
-            <Label htmlFor={fieldName}>{label}</Label>
-            <Input
-              id={fieldName}
-              name={fieldName}
-              type={type}
-              placeholder={placeholder}
-              required={required}
-              defaultValue={value}
-            />
-          </div>
-        )
-      }
-    )
-
-  const renderFormActions = () =>
-    formType === "register" ? (
-      <AuthButton
-        label={title}
-        className="col-span-2 mx-auto w-1/2"
-        action={signup}
-      />
-    ) : (
-      <div className="flex items-center gap-x-4">
-        <AlertDialog>
-          <Button asChild variant="destructive">
-            <AlertDialogTrigger className="mx-auto w-1/4">
-              Cancel
-            </AlertDialogTrigger>
-          </Button>
-          <AlertDialogContent>
-            <AlertDialogHeader>
-              <AlertDialogTitle>Are you sure?</AlertDialogTitle>
-              <AlertDialogDescription>
-                Any changes you may have made will be discarded. Do you still
-                want to exit?
-              </AlertDialogDescription>
-              <AlertDialogFooter>
-                <AlertDialogCancel>Nah</AlertDialogCancel>
-                <Button asChild variant="destructive">
-                  <AlertDialogAction>
-                    <Link href={`/profile/${userData?.id}`}>Yeah</Link>
-                  </AlertDialogAction>
-                </Button>
-              </AlertDialogFooter>
-            </AlertDialogHeader>
-          </AlertDialogContent>
-        </AlertDialog>
-        <AuthButton label="Confirm" action={updateProfile} />
-      </div>
-    )
-
   return (
-    <form className="m-base border bg-background p-8 shadow" autoComplete="off">
-      <fieldset className="grid auto-cols-auto gap-4">
-        <Typography
-          variant="h2"
-          className="col-span-2 mx-auto w-full text-center"
-        >
-          {title}
-        </Typography>
-
-        {renderFormFields()}
-
-        {renderFormActions()}
+    <form
+      className="rounded border bg-background p-8 drop-shadow"
+      autoComplete="off"
+      aria-labelledby="form-title"
+    >
+      <Typography variant="h2">{title}</Typography>
+      <fieldset className="grid grid-cols-1 gap-4 md:grid-cols-2">
+        <legend className="sr-only">User Profile Information</legend>
+        <FormFields formType={formType} userData={userData} />
+        <FormActions formType={formType} title={title} userData={userData} />
       </fieldset>
     </form>
+  )
+}
+
+function FormFields({ formType, userData }: FormFieldsProps) {
+  return (
+    <>
+      {registerFormItems.map(
+        ({
+          type,
+          fieldName,
+          placeholder,
+          label,
+          required = formType === "edit" ? false : true,
+        }) => {
+          const value =
+            formType === "edit"
+              ? userData?.user_metadata[fieldName]
+              : fieldName === "email"
+                ? userData?.email
+                : ""
+
+          const inputId = `${fieldName}-input`
+
+          return (
+            <div
+              key={fieldName}
+              className={cn(
+                "space-y-1",
+                fieldName === "email" ||
+                  fieldName === "password" ||
+                  fieldName === "confirm_password"
+                  ? "md:col-span-2"
+                  : "md:col-span-1"
+              )}
+            >
+              <Label htmlFor={inputId} className="text-sm font-medium">
+                {label}
+              </Label>
+              <Input
+                id={inputId}
+                name={fieldName}
+                type={type}
+                placeholder={placeholder}
+                required={required}
+                defaultValue={value}
+                className="w-full"
+                aria-describedby={`${inputId}-error`}
+              />
+              <div
+                id={`${inputId}-error`}
+                aria-live="polite"
+                className="text-sm text-red-500"
+              ></div>
+            </div>
+          )
+        }
+      )}
+    </>
+  )
+}
+
+function FormActions({ formType, title, userData }: FormActionsProps) {
+  return (
+    <div className="mt-4 flex flex-col items-center justify-center gap-4 sm:flex-row md:col-span-2">
+      {formType === "register" ? (
+        <AuthButton label={title} className="w-full sm:w-1/2" action={signup} />
+      ) : (
+        <>
+          <CancelButton userData={userData} />
+          <AuthButton
+            label="Confirm"
+            action={updateProfile}
+            className="w-full sm:w-1/2"
+          />
+        </>
+      )}
+    </div>
+  )
+}
+
+function CancelButton({ userData }: { userData?: User }) {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="destructive" className="w-full sm:w-1/4">
+          Cancel
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            Any changes you may have made will be discarded. Do you still want
+            to exit?
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>No, go back</AlertDialogCancel>
+          <AlertDialogAction asChild>
+            <Button variant="destructive">
+              <Link href={`/profile/${userData?.id}`}>
+                Yes, discard changes
+              </Link>
+            </Button>
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   )
 }
