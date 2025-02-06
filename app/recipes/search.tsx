9022db850c45563c67044ce5c4e59e2294469411
jccdev45 <jccdev45@gmail.com>
2025-02-06 14:29:52 -0500

    feat: :sparkles: Improve UX for search bar
    - Add functionality for hiding/displaying search results when clicking inside/outside of search bar or results element

--- app/recipes/search.tsx
+++ app/recipes/search.tsx
@@ -1,11 +1,12 @@
 "use client"
 
-import { useMemo, useState } from "react"
+import { useMemo, useRef, useState } from "react"
 import Link from "next/link"
 import { searchRecipes } from "@/queries/recipe-queries"
 import { createClient } from "@/supabase/client"
 import { useQuery } from "@supabase-cache-helpers/postgrest-react-query"
 import { Search } from "lucide-react"
+import { useOnClickOutside } from "usehooks-ts"
 
 import { cn } from "@/lib/utils"
 import { Input } from "@/components/ui/input"
@@ -13,8 +14,10 @@ import { Label } from "@/components/ui/label"
 import { Typography } from "@/components/ui/typography"
 
 export function Searchbar({ className }: { className?: string }) {
-  const supabase = createClient()
   const [searchTerm, setSearchTerm] = useState("")
+  const [showResults, setShowResults] = useState(false)
+  const searchRef = useRef<HTMLDivElement>(null as unknown as HTMLDivElement)
+  const supabase = createClient()
 
   const {
     data: searchResults,
@@ -24,7 +27,18 @@ export function Searchbar({ className }: { className?: string }) {
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(e.target.value)
+    setShowResults(true)
   }
+
+  const handleClickOutside = () => {
+    setShowResults(false)
+  }
+
+  function handleClickInside() {
+    setShowResults(true)
+  }
+
+  useOnClickOutside(searchRef, handleClickOutside)
 
   const renderSearchResults = useMemo(() => {
     if (isLoading) {
@@ -41,16 +55,17 @@ export function Searchbar({ className }: { className?: string }) {
 
     return (
       <>
-        <Typography variant="h4">Showing results for: {searchTerm}</Typography>
-        <ul>
+        <Typography variant="h4" className="p-1">
+          Showing results for: {searchTerm}
+        </Typography>
+        <ul className="">
           {searchResults.map((recipe) => (
-            <li key={recipe.id}>
-              <Link
-                href={`/recipes/${recipe.slug}`}
-                className="block p-2 hover:bg-accent"
-              >
-                {recipe.recipe_name}
-              </Link>
+            <li
+              key={recipe.id}
+              className="block p-2 hover:bg-accent last:hover:rounded-b-md"
+              onClick={handleClickOutside}
+            >
+              <Link href={`/recipes/${recipe.slug}`}>{recipe.recipe_name}</Link>
             </li>
           ))}
         </ul>
@@ -71,13 +86,16 @@ export function Searchbar({ className }: { className?: string }) {
           className="w-full pr-10"
           placeholder="Search recipes, ingredients, etc.."
           onChange={handleChange}
+          onClick={handleClickInside}
           autoComplete="off"
         />
-
         <Search className="absolute right-2 top-0 size-5 h-full" />
       </div>
-      {searchTerm && (
-        <div className="absolute top-full z-10 mt-1 w-full rounded-md bg-muted-foreground text-muted shadow-lg">
+      {showResults && (
+        <div
+          ref={searchRef}
+          className="absolute z-10 mt-1 w-full rounded-md bg-muted-foreground text-muted shadow-lg"
+        >
           {renderSearchResults}
         </div>
       )}
